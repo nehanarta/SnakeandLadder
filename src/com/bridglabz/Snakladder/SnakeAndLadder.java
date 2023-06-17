bd39ce0e846f375026554e391f201e81257b6f38
nehanarta <nehabaswanthrao@gmail.com>
2023-06-17 10:41:28 +0530

    Added:snake and ladder prog

---
+++ src/com/bridglabz/Snakladder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridglabz.Snakladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the snake and ladder");
+
+    }
+}
