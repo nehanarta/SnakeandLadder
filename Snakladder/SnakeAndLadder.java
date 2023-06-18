5708c3f70f5c7cf46f028fc9db23b09d84d96699
nehanarta <nehabaswanthrao@gmail.com>
2023-06-18 19:15:36 +0530

    winning position

--- Snakladder/SnakeAndLadder.java
+++ Snakladder/SnakeAndLadder.java
@@ -1,32 +1,39 @@
 package com.bridglabz.Snakladder;
 
 public class SnakeAndLadder {
+    static int winning_Position = 100;
     static int playerPosition = 0;
 
 
     public static void main(String[] args) {
-        System.out.println("startPosition===>"+playerPosition);
+        System.out.println("startPosition===>" + playerPosition);
         int rollDice = (int) ((Math.random() * 10) % 6 + 1);
-        System.out.println("RollDice====>"+rollDice);
+        System.out.println("RollDice====>" + rollDice);
 
-        int option=(int) ((Math.random() * 10) % 3);
-        System.out.println("option===>"+option);
-        switch(option){
-            case 0:
-                System.out.println("No play");
-                break;
-            case 1:
-                playerPosition=playerPosition+rollDice;
-                System.out.println("ladder==>"+playerPosition);
-                break;
-            case 2:
-                playerPosition=playerPosition-rollDice;
-                System.out.println("snake==>"+playerPosition);
-            default:
-                System.out.println("is invalid input");
-                break;
+        while (playerPosition < winning_Position) {
+            int option = (int) ((Math.random() * 10) % 3);
+            System.out.println("option===>" + option);
+            switch (option) {
+                case 0:
+                    System.out.println("No play");
+                    break;
+                case 1:
+                    playerPosition = playerPosition + rollDice;
+                    if(playerPosition<=winning_Position);
+                    System.out.println("ladder==>" + playerPosition);
+                    break;
+                case 2:
+                    playerPosition = playerPosition - rollDice;
+                    if (playerPosition < 0)
+                        playerPosition = 0;
+                    System.out.println("snake==>" + playerPosition);
+                    break;
+                default:
+                    System.out.println("is invalid input");
+                    break;
+            }
+
+
         }
-
-
     }
 }
