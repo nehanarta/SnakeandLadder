6877d27d43309961ba95540d72bfc4b0a4062f12
nehanarta <nehabaswanthrao@gmail.com>
2023-06-18 11:19:20 +0530

    Added:play rollDice

--- src/com/bridglabz/Snakladder/SnakeAndLadder.java
+++ src/com/bridglabz/Snakladder/SnakeAndLadder.java
@@ -1,10 +1,13 @@
 package com.bridglabz.Snakladder;
 
 public class SnakeAndLadder {
-    static int startPosition=0;
+    static int startPosition = 0;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the snake and ladder");
-        System.out.println(("start position is===>"+startPosition));
+        System.out.println("startPosition===>"+startPosition);
+        int rollDice = (int) ((Math.random() * 10) % 6 + 1);
+        System.out.println("RollDice====>"+rollDice);
+
 
     }
 }
