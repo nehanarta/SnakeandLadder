e26dcbf811054f89de7f895671803c3e2dd9f9bc
nehanarta <nehabaswanthrao@gmail.com>
2023-06-18 11:09:17 +0530

    Added:startPosition

--- src/com/bridglabz/Snakladder/SnakeAndLadder.java
+++ src/com/bridglabz/Snakladder/SnakeAndLadder.java
@@ -1,8 +1,10 @@
 package com.bridglabz.Snakladder;
 
 public class SnakeAndLadder {
+    static int startPosition=0;
     public static void main(String[] args) {
         System.out.println("Welcome to the snake and ladder");
+        System.out.println(("start position is===>"+startPosition));
 
     }
 }
